6c2a9dfe43d5e2795401c9b3a226fc302da2bf41
MaryMiller00 <mary092@revature.net>
2022-12-09 11:08:15 -0500

    submit

--- week4/problem1/src/main/java/com/revature/problem1/Solution.java
+++ week4/problem1/src/main/java/com/revature/problem1/Solution.java
@@ -1,7 +1,40 @@
 package com.revature.problem1;
+import java.util.*;
+import java.util.Arrays;
 
 public class Solution {
     public int elementNotInArray(int[] arr, int k) {
-        throw new RuntimeException("TODO! Needs implementation...");
+       // throw new RuntimeException("TODO! Needs implementation...");
+        //sort array
+       Arrays.sort(arr);
+
+        int min = 0;
+        int max = arr.length -1; 
+        int result = k+1;
+
+        //while loop for finding the middle and searching the elements
+        while(min<=max){//while min is less than or equal to the max
+            //mid
+            int mid = (min+max)/2;
+
+            if(arr[mid]<= result){//if the middle value is less than or equal to k
+                
+                //and if the midle is = to k
+                if(arr[mid] == result){
+                    //searching increased by one
+                    result ++;
+
+                    // max is set
+                    max = arr.length-1;
+                }
+                //the min becomes the middle
+                min = mid +1;
+            }
+            //else we want the max to be the middle
+            else
+                max = mid - 1;
+        }
+        //then we return the element that is greater
+        return result;
     }
 }
--- week4/problem3/src/main/java/com/revature/problem3/Solution.java
+++ week4/problem3/src/main/java/com/revature/problem3/Solution.java
@@ -1,9 +1,38 @@
 package com.revature.problem3;
-
+import java.util.HashMap;
 import java.util.*;
+import java.util.Map;
+
 
 public class Solution {
     public String wordCount(String sentence) {
-        throw new RuntimeException("TODO! Needs implementation...");
+       // throw new RuntimeException("TODO! Needs implementation...");
+            //create a map
+            Map<String, Integer> hash = new HashMap<>();
+            Map<String, Integer> hash2 = new HashMap<>();
+            String[] wrd = sentence.split(" ");
+            //we are splitting the different words into an array
+
+            //while x is less than the array size
+            for(String x : wrd){
+
+                if(hash.containsKey(x))
+                hash.put(x, hash.get(x)+1);
+
+                else
+                
+                 hash.put(x, 1);
+            }
+           
+
+                for(String x : wrd){
+                    if(hash.containsKey(x))
+
+            if(!hash.containsValue(1)){
+                hash2.put(x, hash.get(x)+1);
+            }
+        }
+
+            return hash2.toString();
     }
 }
